73b6fc593adb38e966adce4409f7cdc7891086b9
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2021-04-11 00:40:49 +0700

    Extract metadata block to rollup configuration file

--- rollup.conf.js
+++ rollup.conf.js
@@ -1,7 +1,21 @@
+let bannerText = `
+// ==UserScript==
+// @name         userscript-repo-template
+// @version      0.0.0
+// @description  Repository template for userscript development.
+// @license      MIT
+// @author       murphyne
+// @namespace    https://github.com/murphyne
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+`;
+
 export default {
   input: 'script.user.js',
   output: {
     file: 'dist/script.user.js',
     format: 'esm',
+    banner: bannerText.trimStart(),
   },
 };
--- script.user.js
+++ script.user.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         userscript-repo-template
-// @version      0.0.0
-// @description  Repository template for userscript development.
-// @license      MIT
-// @author       murphyne
-// @namespace    https://github.com/murphyne
-// @match        http://*/*
-// @grant        none
-// ==/UserScript==
-
 (function() {
     'use strict';
 
